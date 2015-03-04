a38eea2bd2815c5f9ba1ae7209a3de7ccc6e926d
vwarship <vwarship@163.com>
2015-03-04 22:52:43 +0800

    编写了 ViewHolder 类，简化Adapter中代码的编写。

--- app/src/main/java/com/zaoqibu/jiegereader/NewsArrayAdapter.java
+++ app/src/main/java/com/zaoqibu/jiegereader/NewsArrayAdapter.java
@@ -5,9 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 import com.zaoqibu.jiegereader.rss.Item;
+import com.zaoqibu.jiegereader.util.ViewHolder;
 
 /**
  * Created by vwarship on 2015/3/3.
@@ -25,46 +25,17 @@ public class NewsArrayAdapter extends ArrayAdapter<Item> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-
-//        if (convertView == null) {
-//            LayoutInflater inflater = LayoutInflater.from(getContext());
-//            view = inflater.inflate(resource, parent, false);
-//        }
-//
-//        Item news = getItem(position);
-//
-//        TextView newsTitle = (TextView)view.findViewById(R.id.tvNewsTitle);
-//        newsTitle.setText(news.getTitle());
-//
-//        TextView newsDate = (TextView)view.findViewById(R.id.tvNewsDate);
-//        newsDate.setText(news.getPubDate());
-
-
-        //View Holder
-        class ViewHolder {
-            TextView title;
-            TextView date;
-        }
-        final ViewHolder viewHolder;
-
         if (convertView == null) {
-            view = inflater.inflate(resource, null);
-
-            viewHolder = new ViewHolder();
-            viewHolder.title = (TextView)view.findViewById(R.id.tvNewsTitle);
-            viewHolder.date = (TextView)view.findViewById(R.id.tvNewsDate);
-
-            view.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder)view.getTag();
+            convertView = inflater.inflate(resource, null);
         }
 
         Item news = getItem(position);
 
-        viewHolder.title.setText(news.getTitle());
-        viewHolder.date.setText(news.getPubDate());
+        final ViewHolder viewHolder = ViewHolder.get(convertView);
+        viewHolder.setText(R.id.tvNewsTitle, news.getTitle());
+        viewHolder.setText(R.id.tvNewsDate, news.getPubDate());
 
-        return view;
+        return convertView;
     }
+
 }
---
+++ app/src/main/java/com/zaoqibu/jiegereader/util/ViewHolder.java
@@ -0,0 +1,43 @@
+package com.zaoqibu.jiegereader.util;
+
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by vwarship on 2015/3/4.
+ */
+public class ViewHolder {
+    private SparseArray<View> views;
+    private View convertView;
+
+    private ViewHolder(View convertView) {
+        this.convertView = convertView;
+        views = new SparseArray<View>();
+    }
+    public static ViewHolder get(View convertView) {
+        ViewHolder viewHolder = (ViewHolder)convertView.getTag();
+        if (viewHolder == null) {
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+
+        return viewHolder;
+    }
+
+    public <T extends View> T getView(int resId) {
+        View view = views.get(resId);
+        if (view == null) {
+            view = convertView.findViewById(resId);
+            views.put(resId, view);
+        }
+
+        return (T) view;
+    }
+
+    public void setText(int resId, String text) {
+        TextView view = getView(resId);
+        view.setText(text);
+    }
+}
